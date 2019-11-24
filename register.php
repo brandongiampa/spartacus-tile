75396b73d67edc0aee22f84457d7fef2a60c7a5a
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2019-11-24 02:37:40 -0500

    create query to insert into mariaDB from register page

--- register.php
+++ register.php
@@ -4,6 +4,42 @@
 <div class="index">
   <?php include_once 'inc/navbar.php';?>
 </div>
+<?php
+
+  $foundAccount = false;
+
+  if(isset($_POST['register_email'])){
+    try{
+      $query = 'SELECT COUNT(*) FROM `account` WHERE email = ?';
+      $stmt = $con->prepare($query);
+      $stmt->execute([$_POST['register_email']]);
+      $count = $stmt->fetchColumn();
+
+    }catch(PDOException $exception){
+      echo 'There was a problem: ' . $exception;
+    }
+    if ($count>0){
+      $foundAccount = true;
+    }
+    else {
+      try{
+        $email = $_POST['register_email'];
+        $password = password_hash($_POST['register_password'], PASSWORD_DEFAULT);
+
+        $query = 'INSERT INTO `account`(email, salt) VALUES(?, ?)';
+        $stmt = $con->prepare($query);
+        $stmt->execute([$email, $password]);
+        echo '<script>alert("Thanks!");</script>';
+      }catch(PDOException $exception){
+        echo '</br></br></br></br></br></br></br></br>' . $exception;
+      }
+
+    }
+  }
+
+
+?>
+
 <script>
   isIndex = false;
   scrollTop = 0;
@@ -12,6 +48,14 @@
 <main class="non-index bg-white" id="login-page">
   <div class="header-sm">
     <h1>Registration Page</h1>
+    <?php
+      if($foundAccount):?>
+        <div class="container">
+          <div class="alert alert-warning">
+            There is already an account using that email address.  <a href="login.php">Log In</a> | <a href="login.php">Forgot Password</a>
+          </div>
+        </div>
+      <?php endif;?>
   </div>
   <div class="container">
     <div class="row">
@@ -27,10 +71,10 @@
             <input type="password" class="form-control" id="register_password" name="register_password">
           </div>
           <div class="form-group">
-            <label for="register_confirm">Password</label>
+            <label for="register_confirm">Confirm Password</label>
             <input type="password" class="form-control" id="register_confirm" name="register_confirm">
           </div>
-          <button type="submit" class="btn btn-primary">Register</button>
+          <button type="submit" class="btn btn-primary" name="register">Register</button>
         </form>
       </div>
     </div>
