13ec614eaed42f966b496a7b751a4a39e792d745
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2019-11-27 23:28:31 -0500

    add account activated function to database

--- php/database.php
+++ php/database.php
@@ -1,22 +1,23 @@
 <?php
-public class Database{
+class Database{
 
   private $host = 'localhost';
   private $username = 'root';
   private $password = '';
   private $db_name = 'spartacus_tile';
-
   private $con;
+
+  public $errorMsg;
 
   function __construct(){
 
   }
 
   public function connect(){
-    private $host = $this->host;
-    private $username = $this->username;
-    private $password = $this->password;
-    private $db_name = $this->db_name;
+    $host = $this->host;
+    $username = $this->username;
+    $password = $this->password;
+    $db_name = $this->db_name;
 
     try {
       $this->con = new PDO("mysql:host={$host}; dbname={$db_name}", $username, $password);
@@ -25,7 +26,7 @@ public class Database{
     }
   }
   public function writeTestimonial($account_id, $first_name, $last_name, $city, $picPath, $title, $text){
-    $query = "INSERT INTO `testimonials` (`account_id`, `first_name`, `last_name`, `pic`, `title`, `text`) VALUES (?,?,?,?,?,?,?)";
+    $query = "INSERT INTO `testimonials` (`account_id`, `first_name`, `last_name`, `city`, `pic`, `title`, `text`) VALUES (?,?,?,?,?,?,?)";
     $stmt = $this->con->prepare($query);
     $array = array($account_id, $first_name, $last_name, $city, $picPath, $title, $text);
     $stmt->execute($array);
@@ -37,7 +38,7 @@ public class Database{
     $stmt->execute($array);
   }
   public function getTestimonial($email){
-    $query = "SELECT * FROM `testimonials` WHERE `account_id` = (SELECT `id` FROM `account` WHERE `email` = ?)";
+    $query = "SELECT * FROM `testimonials` WHERE `account_id` = (SELECT `id` FROM `account` WHERE `email` = ?) LIMIT 1";
     $stmt = $this->con->prepare($query);
     $array = array($email);
     $stmt->execute($array);
@@ -51,6 +52,23 @@ public class Database{
     $stmt->execute($array);
     $account = $stmt->fetch(PDO::FETCH_OBJ);
     return $account;
+  }
+  public function getTestimonialCount($email){
+    $query = 'SELECT COUNT(*) FROM `testimonials` WHERE `account_id` = (SELECT `id` FROM `account` WHERE `email` = ?)';
+    $stmt = $this->con->prepare($query);
+    $array = array($email);
+    $stmt->execute($array);
+    return $stmt->fetchColumn();
+  }
+  public function isAccountActivated($email){
+    $query = 'SELECT `isActivated` FROM `testimonials` WHERE `email` = ?';
+    $stmt = $this->con->prepare($query);
+    $array = array($email);
+    $stmt->execute($array);
+    if ($stmt->isActivated===0){
+      return false;
+    }
+    return true;
   }
 }
 
