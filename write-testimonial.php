57a6e313e36d389e2bac943fd072f9865f434a01
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2019-11-27 23:10:46 -0500

    fix write testimonial functionality

--- write-testimonial.php
+++ write-testimonial.php
@@ -1,18 +1,7 @@
-<?php include_once 'php/database.php'; ?>
-<?php
-
-  $query = "SELECT COUNT(*) FROM `testimonials` WHERE `account_id` = ?";
-  $stmt = $con->prepare($query);
-  $stmt->execute([10]);
-
-?>
-
 <?php include_once 'inc/head.php';?>
 <div class="index">
   <?php include_once 'inc/navbar.php';?>
 </div>
-<?php include_once 'php/validate.php'; ?>
-
 
 <script>
   isIndex = false;
@@ -25,22 +14,51 @@
     <h1>Write Your Testimonial</h1>
   </div>
   <div class="container">
-    <?php include_once 'php/submit-testimonial-query.php'; ?>
+    <?php include_once 'php/database.php'; ?>
+    <?php include_once 'php/functions.php'; ?>
+
+    <?php
+      $db = new Database();
+
+      $db->connect();
+      $account = $db->getAccountInfo('brandongiampa555@gmail.com');
+      $echo = $account->id;
+      $msg = $db->errorMsg;
+
+      if (isset($_POST['submit-testimonial'])){
+        echo 'POSTED';
+        $id = $account->id;
+        $fName = $_POST['first-name'];
+        $lName = $_POST['last-name'];
+        $city = $_POST['city'];
+        $picTempPath = $_FILES['pic']['tmp_name'];
+        $picPath = createPermanentImagePath($_FILES['pic']);
+        $title = $_POST['title'];
+        $text = $_POST['text'];
+
+        $db->writeTestimonial($id, $fName, $lName, $city, $picPath, $title, $text);
+
+        if ($picPath !== ""){
+          move_uploaded_file($picTempPath, $picPath);
+        }
+        header('Location: my-account.php');
+      }
+    ?>
   </div>
   <div class="container">
     <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" enctype="multipart/form-data">
       <div class="form-row">
         <div class="form-group col-12 col-lg-4">
-          <label for="register-first-name">First Name*</label>
-          <input type="text" class="form-control" id="register-first-name" name="register-first-name">
+          <label for="first-name">First Name*</label>
+          <input type="text" class="form-control" id="first-name" name="first-name">
         </div>
         <div class="form-group col-12 col-lg-4">
-          <label for="register-last-name">Last Name*</label>
-          <input type="text" class="form-control" id="register-last-name" name="register-last-name">
+          <label for="last-name">Last Name*</label>
+          <input type="text" class="form-control" id="last-name" name="last-name">
         </div>
         <div class="form-group col-12 col-lg-4">
-          <label for="register-city">City*</label>
-          <select type="text" class="custom-select" id="register-city" name="register-city">
+          <label for="city">City*</label>
+          <select type="text" class="custom-select" id="city" name="city">
             <option selected><span class="text-muted">Please select...</span></option>
             <option value="Madison Heights">Madison Heights</option>
             <option value="Royal Oak">Royal Oak</option>
@@ -50,21 +68,21 @@
       </div>
       <div class="form-row">
         <div class="col-12 col-lg-8">
-          <label for="testimonial-title">Title*</label>
-          <input type="text" name="testimonial-title" id="testimonial-title" class="form-control">
+          <label for="title">Title*</label>
+          <input type="text" name="title" id="title" class="form-control">
         </div>
         <div class="col-12 col-md-4">
-          <label for="testimonial-pic">Upload Picture <span class="text-muted">(Optional)</span></label>
-          <input class="form-control-file" type="file" name="testimonial-pic" id="testimonial-pic">
+          <label for="pic">Upload Picture <span class="text-muted">(Optional)</span></label>
+          <input class="form-control-file" type="file" name="pic" id="pic">
         </div>
       </div>
       <div class="form-row">
         <div class="col-12">
-          <label for="testimonial-text">Text*</label>
-          <textarea class="form-control-file" rows="12" name="testimonial-text" id="testimonial-text"></textarea>
+          <label for="text">Text*</label>
+          <textarea class="form-control-file" rows="12" name="text" id="text"></textarea>
         </div>
       </div>
-      <button type="submit" class="btn btn-primary mt-1" name="submit-testimonial">Submit</button>
+      <input type="submit" class="btn btn-primary mt-1" name="submit-testimonial" value="Submit">
     </form>
   </div>
 </main>
