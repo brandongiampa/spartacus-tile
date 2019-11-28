6f14e5957c5531b3a34248c85b6f3e75fbc5cfd9
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2019-11-27 22:24:14 -0500

    unify validate.php functions

--- php/validate.php
+++ php/validate.php
@@ -1,4 +1,35 @@
 <?php
+function validateImage($img){
+  if($img['error']===4){
+    return true;
+  }
+  if (!validateImageSize($img)){
+    return false;
+  }
+  return validateImageExtension($img);
+}
+function validateImageExtension($img){
+  $acceptedFileExtensions = ['jpg', 'jpeg', 'png'];
+  $fileExtension = explode('.', $img['name']);
+  $fileExtension = strtolower(end($fileExtension));
+
+  if (in_array($fileExtension, $acceptedFileExtensions)){
+    return true;
+  }
+  return false;
+}
+function validateImageSize($img){
+  if ($img['size']>1000000){
+    return false;
+  }
+  return true;
+}
+function createPermanentImagePath($img){
+  $fileExtension = explode('.', $img['name']);
+  $fileExtension = strtolower(end($fileExtension));
+  $destination = 'uploads/'.uniqid('', true).'.'.$fileExtension;
+  return $destination;
+}
 function validateEmail($email){
   if (filter_var($email, FILTER_VALIDATE_EMAIL)){
     return true;
