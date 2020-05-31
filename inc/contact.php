9c612901df21cbb0054729a78d3559b602e7de98
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2020-05-31 02:14:32 -0700

    fix social media links and ugly message box

--- inc/contact.php
+++ inc/contact.php
@@ -6,20 +6,22 @@
       <img src="<?php echo $site_url;?>img/spartacus_tile_icon_white.png" alt="" class="p-5 m-auto">
     </div>
     <div class="container-fluid col-12 col-lg-4 inline m-auto">
-      <div class="container m-auto" id="address">
-        <h3>Spartacus Tile</h3>
-        <p><b>Location</b></br>
-          Address: 1608 E Lincoln Ave, Madison Heights, MI 48071</br>
-        Phone Number:  (248) 954-0594</br>
-        Fax Number: (248) 954-0595</br>
-        <br>
-        <b>Business Hours</b></br>
-          Monday through Friday: 9am-5pm</br>
-          Saturday: 9am-2pm</br>
-          Sunday: CLOSED
-        </p>
-        <h5>PROUDLY SERVING OAKLAND COUNTY SINCE 1980!</H5>
-        <nav id="social-nav">
+      <div class="container m-auto d-flex flex-column justify-content-center align-items-center" id="address">
+        <div class="wrapper">
+          <h3>Spartacus Tile</h3>
+          <p><b>Location</b></br>
+            Address: 1608 E Lincoln Ave, Madison Heights, MI 48071</br>
+          Phone Number:  (248) 954-0594</br>
+          Fax Number: (248) 954-0595</br>
+          <br>
+          <b>Business Hours</b></br>
+            Monday through Friday: 9am-5pm</br>
+            Saturday: 9am-2pm</br>
+            Sunday: CLOSED
+          </p>
+          <h5>PROUDLY SERVING OAKLAND COUNTY SINCE 1980!</H5>
+        </div>
+        <nav id="social-nav" class="mt-4">
           <ul>
             <li>
               <a href="http://www.facebook.com/spartacustile">
@@ -45,7 +47,7 @@
         </nav>
       </div>
     </div>
-    <div class="col-12 col-lg-5 row justify-content-center" id="contact-form">
+    <div class="col-12 col-lg-5 row d-flex flex-column align-items-start px-4 mr-2" id="contact-form">
       <?php include_once 'contact-form.php';?>
     </div>
   </div>
